5073bd1ba4868c96802dc27bc01190f91b9cb412
RKanna <rkannan.balakrishnan@hotmail.com>
2023-06-25 14:46:18 +0530

    done fully

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
 "use strict";
+const sectionSlider = document.querySelector(".slider-main-container");
+const imgContainer = document.getElementById("slideContainer");
+const imagesAll = document.querySelectorAll(".image1");
+const buttonsAll = document.querySelectorAll(".button");
+
+let imageIndex = 0;
+
+const slideImage = () => {
+  imgContainer.style.transform = `translate(-${imageIndex * 100}%)`;
+};
+
+const updateClick = (e) => {
+  const targetId = e.target.id;
+  if (targetId === "previous") {
+    imageIndex = imageIndex === 0 ? imagesAll.length - 1 : imageIndex - 1;
+  } else if (targetId === "next") {
+    imageIndex = imageIndex === imagesAll.length - 1 ? 0 : imageIndex + 1;
+  }
+  slideImage();
+};
+
+buttonsAll.forEach((button) => button.addEventListener("click", updateClick));
+
+sectionSlider.addEventListener("mouseover", () => {
+  buttonsAll.forEach((button) => button.style.display = "block");
+});
+
+sectionSlider.addEventListener("mouseleave", () => {
+  buttonsAll.forEach((button) => button.style.display = "none");
+});
